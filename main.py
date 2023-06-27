e9a5d8e280e64805a9fa530a328b3eb670f5f1bc
procodingclass <88050205+procodingclass@users.noreply.github.com>
2023-06-27 21:39:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,25 +8,24 @@ path = 'Pneumothorax-New-Dataset'
 
 img = cv2.imread("Pneumothorax-New-Dataset/1_image_100.jpg")
 
-# Load the age detection model
+# Load the model
 model = load_model('model_10epochs.h5', compile=False)
 
 try:    
     resizedImg = cv2.resize(img, (200, 200))
     resizedImg = np.array([resizedImg])
 
-    # Pridict the age using age detection model and save it in variable named prediction
+    # Pridict if the image is infected or not and save it in variable 'prediction'
     prediction = model.predict(resizedImg)
 
-   
-    img = cv2.rectangle(img, (10, 10), (300, 100), (255, 255, 255), -1)
+    # If prediction is less then 0.5 then save "infected" in variable 'text" else save "uninfected"
     if prediction[0][0] < 0.5:
         text = "infected"
     else:
         text ="Uninfected"
     
-    print(prediction)
-    # Add prediction[0][0] i.e age of the detected face on the screen  
+    img = cv2.rectangle(img, (10, 10), (300, 100), (255, 255, 255), -1)
+    
     img = cv2.resize(img, (300, 400))  
     img = cv2.putText(img, text, (10, 20), cv2.FONT_HERSHEY_DUPLEX, 0.4, (225, 0, 0), 1)
       
